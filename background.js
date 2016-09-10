154f580ff07020475ee208d9a8b90b73d026592c
lesion <lesion@autistici.org>
2016-09-10 22:29:02 +0200

    upgrade remoteStorage and init hacking

--- background.js
+++ background.js
@@ -1,27 +1,18 @@
 
-console.log('remoteStorage', remoteStorage);
 
-// close auth dialog, once it's finished.
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo) {
-  var authTabId = localStorage.getItem('auth-tab-id');
-  if(authTabId && (tabId == authTabId)) {
-    // only close, if URL also ends on #. That signals, that the access_token
-    // has been recovered from fragment.
-    if(changeInfo.url && changeInfo.url.match(/^chrome\-extension:\/\/.*#$/)) {
-      chrome.tabs.remove(Number(authTabId));
-      localStorage.removeItem('auth-tab-id')
-    }
-  }
-});
+// window.onload = function() {
+//   var div = document.createElement('div');
+//   div.setAttribute('id', 'remotestorage-connect');
+//   document.body.appendChild(div);
 
-window.onload = function() {
-  var div = document.createElement('div');
-  div.setAttribute('id', 'remotestorage-connect');
-  document.body.appendChild(div);
+//   remoteStorage.defineModule('bookmarks', function() { return { exports: {} } });
 
-  remoteStorage.defineModule('bookmarks', function() { return { exports: {} } });
+//   remoteStorage.access.claim('bookmarks','rw');
+//   remoteStorage.displayWidget('remotestorage-connect');
 
-  remoteStorage.claimAccess({'bookmarks':'rw'});
-  remoteStorage.displayWidget('remotestorage-connect');
+// }
+// 
+chrome.commands.onCommand.addListener(function(command) {
+  console.log('bk Command:', command);
+})
 
-}
--- popup.js
+++ popup.js
@@ -1,127 +1,111 @@
+window.addEventListener('DOMContentLoaded', function () {
+  console.error('DOM content loaded ! ')
 
-window.addEventListener('DOMContentLoaded', function() {
+  // var remoteStorage = new RemoteStorage({
+  //   logging: true,  // defaults to false
+  // })
 
-  remoteStorage.defineModule('bookmarks', function(privateClient) {
+  // remoteStorage.access.claim('bookmarks','rw')
 
-    privateClient.use('');
+  // remoteStorage.
 
-    function buildId(string) {
-      return Sha1.hex_sha1(string);
-    }
-
-    return {
-
-      exports: {
-
-        deactivateSync: privateClient.deactivateSync,
-        activateSync: privateClient.activateSync,
-
-        // Add or update a bookmark.
-        //
-        // Parameters:
-        //   path  - path to the bookmark (without the final part), '' for root.
-        //   title - human readable title of the bookmark.
-        //   url   - URL to link to. must be unique.
-        //   index - position within "path", to display at in lists.
-        store: function(parentPath, title, url, index, cb) {
-          var path = parentPath + buildId(url);
-          privateClient.storeObject('bookmark', path, {
-            title: title,
-            url: url,
-            index: index
-          }, cb);
-        },
-
-        createDirectory: function(parentPath, title, index, cb) {
-          var dirPath = parentPath + buildId(title) + '/';
-          var path = dirPath + '_meta';
-          privateClient.storeObject('directory-meta', path, {
-            title: title,
-            index: index,
-          }, function() { cb(dirPath); });
-        },
-
-        syncNow: function() {
-          privateClient.syncNow('');
-        }
-      }
-
-    }
-
-  });
-
-  function pushAll(allDone) {
-    function pushOne(node, parentPath, cb) {
-      if(node.url) {
-        remoteStorage.bookmarks.store(parentPath, node.title, node.url, node.index, cb);
-      } else {
-        var i = 0, dirPath;
-        var pushIter = function() {
-          if(i < node.children.length) {
-            pushOne(node.children[i++], dirPath, pushIter);
-          } else {
-            allDone();
-          }
-        }
-        remoteStorage.bookmarks.createDirectory(
-          parentPath, node.title, node.index,
-          function(p) { dirPath = p; pushIter(); }
-        );
-      }
-    }
-
-    chrome.bookmarks.getTree(function(tree) {
-      var root = tree[0];
-
-      remoteStorage.bookmarks.deactivateSync();
-
-      for(var i=0;i<root.children.length;i++) {
-        pushOne(root.children[i], '');
-      }
-
-      remoteStorage.bookmarks.activateSync();
-      remoteStorage.bookmarks.syncNow();
-
-    });
+  // remoteStorage.displayWidget('remotestorage-connect')
+  // chrome.tabs.create({url: 'http://autistici.org'}, function(tab) {
+  // localStorage.setItem('auth-tab-id', tab.id)
+  // })
+  function getParameterByName (url, name) {
+    var match = RegExp('[?&#]' + name + '=([^&]*)').exec(url.search)
+    return match && decodeURIComponent(match[1].replace(/\+/g, ' '))
   }
+  var url = 'https://mystuff.ns0.it/rs/oauth/les?client_id=ciao&response_type=token&scope=bookmarks&redirect_uri=' + chrome.identity.getRedirectURL()
+  chrome.identity.launchWebAuthFlow({url: url, interactive: true}, (ret) => {
+    console.error(ret)
+    console.error(getParameterByName(ret,'access_token'))
+    remoteStorage.remote.configure({
+      token: getParameterByName(ret,'access_token')
+    })
+  })
+  // remoteStorage.connect('les@mystuff.ns0.it/rs')
+  // remoteStorage.on('ready', console.log.bind(console.log) )
+  // remoteStorage.on('connected', console.log.bind(console.log) )
+  // remoteStorage.on('disconnected', console.log.bind(console.log) )
 
-  function pullAll() {
-    var note = document.getElementById('not-impl');
-    note.style.display = 'block';
-    setTimeout(function() {
-      note.style.display = 'none';
-    }, 2500);
-  }
+})
 
-  remoteStorage.claimAccess({'bookmarks':'rw'});
+//   console.error(remoteStorage)
 
-  remoteStorage.displayWidget('remotestorage-connect', {
-    syncShortcut: false,
-    authDialog: function(url) {
-      chrome.tabs.create({url: url}, function(tab) {
-        localStorage.setItem('auth-tab-id', tab.id);
-      });
-    }
-  });
+//   function pushAll(allDone) {
+//     function pushOne(node, parentPath, cb) {
+//       if(node.url) {
+//         remoteStorage.bookmarks.store(parentPath, node.title, node.url, node.index, cb)
+//       } else {
+//         var i = 0, dirPath
+//         var pushIter = function() {
+//           if(i < node.children.length) {
+//             pushOne(node.children[i++], dirPath, pushIter)
+//           } else {
+//             allDone()
+//           }
+//         }
+//         remoteStorage.bookmarks.createDirectory(
+//           parentPath, node.title, node.index,
+//           function(p) { dirPath = p; pushIter(); }
+//         )
+//       }
+//     }
 
-  var connected = ( remoteStorage.getWidgetState() == 'connected' ||
-                    remoteStorage.getWidgetState() == 'busy' );
+//     chrome.bookmarks.getTree(function(tree) {
+//       var root = tree[0]
 
-  document.getElementById(
-    connected ? 'connected-info' : 'intro'
-  ).style.display = 'block';
+//       remoteStorage.bookmarks.deactivateSync()
 
-  if(connected) {
-    document.getElementById('push-all').addEventListener('click', function(event) {
-      event.target.setAttribute('disabled', true);
-      pushAll(function() {
-        event.target.setAttribute('disabled', false);
-        var succ = document.getElementById('succ');
-        succ.style.display = 'block';
-        setTimeout(function() { succ.style.display = 'none'; }, 2500);
-      });
-    });
-    document.getElementById('pull-all').addEventListener('click', pullAll);
-  }
+//       for(var i=0;i<root.children.length;i++) {
+//         pushOne(root.children[i], '')
+//       }
 
-});
+//       remoteStorage.bookmarks.activateSync()
+//       remoteStorage.bookmarks.syncNow()
+
+//     })
+//   }
+
+//   function pullAll() {
+//     var note = document.getElementById('not-impl')
+//     note.style.display = 'block'
+//     setTimeout(function() {
+//       note.style.display = 'none'
+//     }, 2500)
+//   }
+
+//   remoteStorage.access.claim('bookmarks','rw')
+
+//   remoteStorage.displayWidget('remotestorage-connect', {
+//     syncShortcut: false,
+//     authDialog: function(url) {
+//       chrome.tabs.create({url: url}, function(tab) {
+//         localStorage.setItem('auth-tab-id', tab.id)
+//       })
+//     }
+//   })
+
+//   var connected = ( remoteStorage.getWidgetState() == 'connected' ||
+//                     remoteStorage.getWidgetState() == 'busy' )
+
+//   document.getElementById(
+//     connected ? 'connected-info' : 'intro'
+//   ).style.display = 'block'
+
+//   if(connected) {
+//     document.getElementById('push-all').addEventListener('click', function(event) {
+//       event.target.setAttribute('disabled', true)
+//       pushAll(function() {
+//         event.target.setAttribute('disabled', false)
+//         var succ = document.getElementById('succ')
+//         succ.style.display = 'block'
+//         setTimeout(function() { succ.style.display = 'none'; }, 2500)
+//       })
+//     })
+//     document.getElementById('pull-all').addEventListener('click', pullAll)
+//   }
+
+// })
