5b7dc3bf5d90d746a6a4de8b8e2ed35bb46bf2ee
lesion <lesion@autistici.org>
2016-10-03 18:43:30 +0200

    add some utility (isUrl, extractToken)

--- app/util.js
+++ app/util.js
@@ -1,19 +1,28 @@
-export default {
+module.exports = {
   
+  extractToken (url) {
+    // https://hpjohjhlkbabmpfmfbgeefnaafipanhd.chromiumapp.org/#access_token=YqT9ddr%2FkFHCA321CKNp%2B3Lxgx8%3D&token_type=bearer&state=
+    return url.match(/#access_token=([^&?]*)/)[1]
+  },
+
+  isUrl (url) {
+    return (url.match(/^http[s]?:\/\//) !== null)
+  },
+
   // get/set extension options
   option: {
     get (name) {
       return new Promise( (resolve, reject) => {
-        chrome.storage.get(name, items => {
-          if (chrome.runtime.lastError) {
+        chrome.storage.local.get(name, items => {
+          if (chrome.runtime.lastError || !items.name)
             reject(new Error(chrome.runtime.lastError))
-          }
-          resolve(items)
+          else
+            resolve(items)
         })
       })
     },
     set (name, value) {
-      chrome.storage.set(name, value)
+      chrome.storage.local.set({name: value})
     }
   }
 }
