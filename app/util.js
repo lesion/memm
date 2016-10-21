9bd70e1cad922ba07494a94488b0274085825140
lesion <lesion@autistici.org>
2016-10-22 00:30:10 +0200

    add useful utility

--- app/util.js
+++ app/util.js
@@ -1,10 +1,39 @@
+/* global chrome */
+import intersection from 'lodash.intersection'
+
 module.exports = {
-  
+  // convert bookmarks into chrome omnibar suggestions
+  bookmarks2suggestion (tags, bookmarks) {
+    return bookmarks.map(b => {
+      const foundTags = intersection(tags, b.tags)
+      return {
+        content: b.url,
+        description: `<url>${b.url.replace('&', '&amp;')}</url> <match>${foundTags.join(', ')}</match>`
+      }
+    })
+  },
+
+  // navigate to url
+  setCurrentTabUrl (url) {
+    chrome.tabs.query({ currentWindow: true, active: true },
+      tabs => { chrome.tabs.update(tabs[0].id, {url}) })
+  },
+
+  // get current tab info !
+  getCurrentTabInfo () {
+    return new Promise((resolve, reject) => {
+      chrome.tabs.query({ currentWindow: true, active: true },
+        tabs => {
+          resolve({ url: tabs[0].url, title: tabs[0].title })
+        })
+    })
+  },
+  // extract oauth token
   extractToken (url) {
-    // https://hpjohjhlkbabmpfmfbgeefnaafipanhd.chromiumapp.org/#access_token=YqT9ddr%2FkFHCA321CKNp%2B3Lxgx8%3D&token_type=bearer&state=
     return url.match(/#access_token=([^&?]*)/)[1]
   },
 
+  // check if is url
   isUrl (url) {
     return (url.match(/^http[s]?:\/\//) !== null)
   },
@@ -12,12 +41,13 @@ module.exports = {
   // get/set extension options
   option: {
     get (name) {
-      return new Promise( (resolve, reject) => {
+      return new Promise((resolve, reject) => {
         chrome.storage.local.get(name, items => {
-          if (chrome.runtime.lastError || !items.name)
+          if (chrome.runtime.lastError || !items[name]) {
             reject(new Error(chrome.runtime.lastError))
-          else
+          } else {
             resolve(items)
+          }
         })
       })
     },
