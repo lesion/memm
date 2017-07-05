6b9c0c6f7eaa14011404606a46946ae7bec8b0aa
lesion <lesion@autistici.org>
2017-07-05 21:46:51 +0200

    refactoring

--- app/popup.js
+++ app/popup.js
@@ -2,7 +2,6 @@ import Vue from 'vue/dist/vue'
 const popup = require('./popup.vue')
 
 const vm = new Vue({
-  el: '#mount',
   components: {popup},
-  render: h => h('popup')
-})
+  render: h => h(popup)
+}).$mount('#mount')
