a1de12653f0dd9fac49acb1ea2e8c5d53a4e1a8d
lesion <lesion@autistici.org>
2017-07-05 21:46:02 +0200

    refactoring

--- app/options.js
+++ app/options.js
@@ -2,7 +2,6 @@ import Vue from 'vue/dist/vue'
 const options = require('./options.vue')
 
 const vm = new Vue({
-  el: '#mount',
   components: {options},
-  render: h => h('options')
-})
+  render: h => h(options)
+}).$mount('#mount')
