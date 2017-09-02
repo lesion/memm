c54817d0458a1565c271b2684275dfb6e3bd552b
lesion <lesion@autistici.org>
2017-09-02 23:39:55 +0200

    use buble

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,10 @@ var webpack = require('webpack')
 var CopyWebpackPlugin = require('copy-webpack-plugin')
 var packageJson = require('./package.json')
 
+const bubbleOptions = {
+  presets: [['es2015', { loose: true, modules: false }]]
+}
+
 module.exports = {
   entry: {
     options: './app/options.js',
@@ -14,7 +18,15 @@ module.exports = {
     filename: '[name].js'
   },
   plugins: [
-    new webpack.optimize.UglifyJsPlugin({minimize:true}),
+     new webpack.LoaderOptionsPlugin({
+       vue: {
+         js: {
+           buble: bubbleOptions
+         }
+       },
+       buble: bubbleOptions
+     }),
+    new webpack.optimize.UglifyJsPlugin({compress: { warnings: false}, output: { comments: false }, minimize: true, sourceMap: false}),
     new webpack.DefinePlugin({  ENV: {
         RELEASE: '"' + packageJson.version + '"'
       }
@@ -29,13 +41,12 @@ module.exports = {
   module: {
     loaders: [
       { test: /\.json$/, loader: 'json-loader' },
-      { test: /\.js$/, exclude: /node_modules|dist/, loader: 'babel-loader?presets[]=es2015' },
+      { test: /\.js$/, exclude: /(node_modules|dist)/, loader: 'buble-loader', query: bubbleOptions },
       { test: /\.vue$/, loader: 'vue-loader', exclude: /node_modules/ },
       { test: /\.png$/, loader: 'file-loader?name=[path][name].[ext]' },
       { test: /\.css$/, loader: 'style-loader!css-loader' },
     ]
   },
-  devtool: 'source-map',
   resolve: {
     extensions: ['.js', '.vue']
   },
