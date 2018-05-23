3bd43d7827c4949523251e94d157baf43811a17a
lesion <lesion@autistici.org>
2018-05-23 23:58:18 +0200

    cleaning

--- log_server.js
+++
@@ -1,8 +0,0 @@
-var WebSocketServer = require('ws').Server
-var wss = new WebSocketServer({ port: 8080 })
-
-wss.on('connection', function connection (ws) {
-  ws.on('message', function incoming (message) {
-    console.log(message)
-  })
-})
